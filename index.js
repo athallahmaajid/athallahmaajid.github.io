01e2460415d86ea11a4b6dcd892e7e9fb12eb83f
athallahmaajid <athallahmaajid@gmail.com>
2022-03-17 20:57:37 +0700

    change nav and add contacts + footer

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ let body = document.getElementsByTagName("body")[0];
 let nav = document.getElementsByTagName("nav")[0];
 
 function goTo(coordinate) {
+  closeNav();
   body.scrollTo({top: coordinate, behavior: "smooth"});
 }
 
 body.addEventListener("scroll", function() {
-  console.log(body.scrollTop);
   if (body.scrollTop == 0) {
     nav.className = "nav-show";
   } else {
     nav.className = "nav-stick";
   }
 });
+
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
